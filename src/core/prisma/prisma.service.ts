a265c27acbfdd8553834fb9af0791431ff934f8c
gustavo brun <gustavobdossantos.gb@gmail.com>
2026-01-08 15:03:58 -0300

    fix: solve prisma service sasl authentication error by instantiating pg pool manually

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -1,18 +1,35 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 
 import { PrismaClient } from './generated/client';
-
+import { Pool } from 'pg';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private pool: Pool;
+
   constructor() {
-    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('[PrismaService] DATABASE_URL not found.');
+    }
+
+    const poolInstance = new Pool({ connectionString });
+
+    const adapter = new PrismaPg(poolInstance);
 
     super({ adapter });
+
+    this.pool = poolInstance;
   }
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    await this.pool.end();
   }
 }
