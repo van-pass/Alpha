18457e8ce25aaf2d8122ff44ae620937709edbb7
gustavo brun <gustavobdossantos.gb@gmail.com>
2026-02-27 19:08:44 -0300

    fix: properly handle cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
